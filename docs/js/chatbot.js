c09c78cc2263415aa9e97cba381c8e13071c2fc7
Mpho-CapaCiTi <mpho.masibigiri@capaciti.org.za>
2024-11-27 16:28:13 +0200

    Added chatbot open and close functionality

---
+++ docs/js/chatbot.js
@@ -0,0 +1,11 @@
+//ChatBot Functionality
+
+//Open Chatbot Function
+function OpenChatFunc(){
+    document.getElementById('chatWindow').style.display = 'flex';
+}
+
+//Close Chatbot Function
+function CloseChatFunc(){
+    document.getElementById('chatWindow').style.display = 'none';
+}
