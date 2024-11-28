4339b6f23e03f087f2f8d0d18ce2d666c716cea8
Mpho-CapaCiTi <mpho.masibigiri@capaciti.org.za>
2024-11-28 09:19:43 +0200

    Added empty field validation functionality

--- docs/js/chatbot.js
+++ docs/js/chatbot.js
@@ -9,3 +9,16 @@ function OpenChatFunc(){
 function CloseChatFunc(){
     document.getElementById('chatWindow').style.display = 'none';
 }
+
+//Empty Message Validation
+function validateFormFunc(){
+    var input = document.getElementById('userinput').value;
+
+    if(input == "")
+    {
+        alert("All fields must be filled out.");
+        return false
+    }
+    return true;
+}
+
