f4cd78bb3fd6dea6c370723cdaedd1b20ef2411b
Mpho-CapaCiTi <mpho.masibigiri@capaciti.org.za>
2024-11-28 10:21:49 +0200

    Added Chatbot Functionality to send, receive and display message

--- docs/js/chatbot.js
+++ docs/js/chatbot.js
@@ -3,22 +3,76 @@
 //Open Chatbot Function
 function OpenChatFunc(){
     document.getElementById('chatWindow').style.display = 'flex';
+    document.getElementById('userinput').focus();
 }
 
 //Close Chatbot Function
 function CloseChatFunc(){
     document.getElementById('chatWindow').style.display = 'none';
+    chatWindowContent.innerHTML = '';
+    userinput.value = '';
 }
 
-//Empty Message Validation
-function validateFormFunc(){
-    var input = document.getElementById('userinput').value;
+// //Empty Message Validation
+// function validateFormFunc(){
+//     const input = document.getElementById('userinput').value;
 
-    if(input == "")
-    {
-        alert("All fields must be filled out.");
-        return false
+//     if(input == "")
+//     {
+//         alert("All fields must be filled out.");
+//         return false;
+//     }
+//     return true;
+// }
+
+function displayMessageFunc(message, sender){
+    const chatwc = document.getElementById('chatWindowContent');
+    const content = document.createElement('div');
+
+    content.classList.add('txtMessages', sender);
+    content.textContent = message;
+
+    chatwc.appendChild(content);
+    chatwc.scrollTop = chatwc.scrollHeight;
+}
+
+function botResponseFunc(userinput){
+    const lowercase = userinput.toLowerCase();
+
+    if(lowercase.includes('hello') || lowercase.includes('hi')) {
+        return "Hello, How can i assist you today?";
+    }if(lowercase.includes('')) {
+        return "Please type a message";
+    }else{
+        return "Sorry, I didn't understand that. Couldyou please rephrase?";
     }
-    return true;
 }
+
+function sendMessageFunc(){
+    const input = document.getElementById('userinput').value.trim();
+
+    if(input === ''){
+
+        alert("All fields must be filled out.");
+        return;
+    }
+
+    displayMessageFunc(input, 'userMessage');
+
+    document.getElementById('userinput').value = '';
+
+    setTimeout(() => {
+        const response = botResponseFunc(input);
+        displayMessageFunc(response, 'botChat');
+    }, 1200);
+}
+
+document.getElementById('userinput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      
+    //   alert('You pressed Enter: ' + this.value);
+      sendMessageFunc();
+    }
+  });
 
