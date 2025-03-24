41412fadad1afc3eec64858a037ab08574691930
MasbiMpho <mpholin15@gmail.com>
2025-03-24 13:47:46 +0200

    Added hamburger for mobile display

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
 // Dark Mode
 const dayNight = document.querySelector(".day-night");
 
-// dayNight.addEventListener("click", () => {
-//     document.body.classList.toggle("dark");
-//     if(document.body.classList.contains("dark"))
-//     {
-//         dayNight.querySelector("i").classList.remove("fa-moon");
-//         dayNight.querySelector("i").classList.add("fa-sun");
-//     }
-//     else
-//     {
-//         dayNight.querySelector("i").classList.remove("fa-sun");
-//         dayNight.querySelector("i").classList.add("fa-moon");
-//     }
-// })
-
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
@@ -46,27 +32,31 @@ const nav = document.querySelector(".nav"),
     totalNavList = navList.length,
     allSection = document.querySelectorAll(".section"),
     totalSection = allSection.length;
+
     for(let i=0; i<totalNavList; i++)
     {
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function()
         {
-            for(let i=0; i<totalSection; i++)
-            {
-                allSection[i].classList.remove("back-section");
-            }
+            removeBackSection();
             for(let j=0; j<totalNavList; j++)
             {
                 if(navList[j].querySelector("a").classList.contains("active"))
                 {
-                    allSection[j].classList.add("back-section");
+                    addBackSection(j);
+                    //allSection[j].classList.add("back-section");
                 }
                 navList[j].querySelector("a").classList.remove("active");
             }
             this.classList.add("active");
             showSection(this);
+            if(window.innerWidth < 1200)
+            {
+                asideSectionTogBtn();
+            }
         })
     }
+
     function showSection(element)
     {
         for(let i=0; i<totalSection; i++)
@@ -77,23 +67,37 @@ const nav = document.querySelector(".nav"),
         document.querySelector("#" + target).classList.add("active")
     }
 
-    const navTogBtn = document.querySelector(".nav-tog"),
+        const navTogBtn = document.querySelector(".nav-tog"),
         aside = document.querySelector(".aside");
         navTogBtn.addEventListener("click", () => 
-        {
-            asideSectionTogBtn();
-        })
-
-        function asideSectionTogBtn()
-        {
-            aside.classList.toggle("open");
-            navTogBtn.classList.toggle("open");
-            for(let i=0; i<totalSection; i++)
             {
-                allSection[i].classList.toggle("open");
-            }
-           
+                asideSectionTogBtn();
+            })
+
+    function removeBackSection()
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("back-section");
         }
+    }
+
+    function asideSectionTogBtn()
+    {
+        aside.classList.toggle("open");
+        navTogBtn.classList.toggle("open");
+
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.toggle("open");
+        }
+           
+    }
+
+    function addBackSection(num)
+    {
+        allSection[num].classList.add("back-section");
+    }
 
 
     //allSection = document.querySelectorAll(".section"),
